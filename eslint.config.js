cfb7bafa8a847996e8aba9ce7f57f8204b480cea
jespinel <jespinelp.d@outlook.com>
2025-08-09 14:36:20 -0500

    🔧 chore(init): bootstrap Astro minimal template

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import astro from "eslint-plugin-astro";
+import astroParser from "astro-eslint-parser";
+import tseslint from "typescript-eslint";
+
+export default [
+  { ignores: ["dist", "node_modules"] },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: [".astro"],
+      },
+    },
+    plugins: { astro },
+    rules: {
+      ...astro.configs.recommended.rules,
+    },
+  },
+];
