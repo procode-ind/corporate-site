817d6ce5148d16de038e6c7842a5ee93e911640e
jespinel <jespinelp.d@outlook.com>
2025-08-09 15:08:10 -0500

    format(prettier): add husky and formatter

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,21 @@
-import js from "@eslint/js";
-import astro from "eslint-plugin-astro";
-import astroParser from "astro-eslint-parser";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import astro from 'eslint-plugin-astro';
+import astroParser from 'astro-eslint-parser';
+import tseslint from 'typescript-eslint';
 
 export default [
-  { ignores: ["dist", "node_modules"] },
+  { ignores: ['dist', 'node_modules'] },
 
   js.configs.recommended,
   ...tseslint.configs.recommended,
 
   {
-    files: ["**/*.astro"],
+    files: ['**/*.astro'],
     languageOptions: {
       parser: astroParser,
       parserOptions: {
         parser: tseslint.parser,
-        extraFileExtensions: [".astro"],
+        extraFileExtensions: ['.astro'],
       },
     },
     plugins: { astro },
---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  astroAllowShorthand: true,
+  astroSortAttributes: 'ascending',
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
